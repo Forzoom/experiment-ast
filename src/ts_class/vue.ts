7039001be74c263f45cc0af51213887e7cde5557
Forzoom <forzoom@163.com>
2020-02-20 14:31:46 +0800

    fix

--- src/ts_class/vue.ts
+++ src/ts_class/vue.ts
@@ -14,6 +14,7 @@ import {
     any,
     getScriptContent,
 } from '@/utils';
+import { VueNode, DataNode, PropNode, ComputedNode, WatchNode, MethodNode } from '@/gen/node';
 
 /**
  * 对于vue文件进行处理
@@ -31,6 +32,7 @@ export default function(input: string, output: string) {
         console.warn(input + ' script is lost');
         return;
     }
+
     const originalAst = recast.parse(scriptContent.jsScript, {
         parser: {
             parse(source: string, options: any) {
@@ -38,6 +40,8 @@ export default function(input: string, output: string) {
                     plugins: [
                         'estree',
                         'decorators-legacy',
+                        'typescript',
+                        'classProperties',
                     ],
                     tokens: true,
                 }))
@@ -52,19 +56,81 @@ export default function(input: string, output: string) {
     // 寻找class的导出
     const body = originalAst.program.body as namedTypes.Node[];
     let classDeclaration: namedTypes.ClassDeclaration | null = null;
+    let importDeclarations: namedTypes.ImportDeclaration[] = [];
+    let other: namedTypes.Node[] = [];
     for (const node of body) {
         if (node.type === 'ClassDeclaration') {
             classDeclaration = node as namedTypes.ClassDeclaration;
+        } else if (node.type === 'ImportDeclaration') {
+            importDeclarations.push(node as namedTypes.ImportDeclaration);
+        } else {
+            other.push(node);
         }
     }
 
-    if (!classDeclaration) {
+    if (!classDeclaration || !classDeclaration.decorators) {
         console.warn('cannot find class declaration');
         return;
     }
 
     // 寻找所有的变量定义
-    for (const item of classDeclaration.body.body) {
-
+    const componentArgument = ((classDeclaration.decorators[0].expression as namedTypes.CallExpression).arguments[0] as namedTypes.ObjectExpression);
+    let className: string | null = null;
+    let componentsList: namedTypes.Property | null = null;
+    for (const property of componentArgument.properties as namedTypes.Property[]) {
+        const name = (property.key as namedTypes.Identifier).name;
+        if (name === 'name') {
+            className = (property.value as namedTypes.StringLiteral).value;
+        } else if (name === 'components') {
+            componentsList = property;
+        }
     }
+    const propNodes: PropNode[] = [];
+    const dataNodes: DataNode[] = [];
+    const computedNodes: ComputedNode[] = [];
+    const watchNodes: WatchNode[] = [];
+    const methodNodes: MethodNode[] = [];
+
+    for (const item of classDeclaration.body.body) {
+        if (item.type === 'ClassProperty') {
+            if (item.decorators) {
+                // Prop
+                const node = new PropNode((item.key as namedTypes.Identifier).name, (item.decorators[0].expression as namedTypes.CallExpression).arguments[0]);
+                node.comments = item.comments;
+                propNodes.push(node);
+            } else {
+                // data
+                const node = new DataNode((item.key as namedTypes.Identifier).name, item.value);
+                node.comments = item.comments;
+                dataNodes.push(node);
+            }
+        } else if (item.type === 'MethodDefinition') {
+            if (item.kind === 'get') {
+                // computed
+                const node = new ComputedNode((item.key as namedTypes.Identifier).name, item.value as namedTypes.FunctionExpression);
+                node.comments = item.comments;
+                computedNodes.push(node);
+            } else {
+                if (item.decorators) {
+                    // watch
+                    const name = (item.decorators[0].expression as namedTypes.CallExpression).arguments[0] as namedTypes.StringLiteral;
+                    const node = new WatchNode(name.value, item.value as namedTypes.FunctionExpression);
+                    node.comments = item.comments;
+                    watchNodes.push(node);
+                } else {
+                    // method
+                    const node = new MethodNode((item.key as namedTypes.Identifier).name, item.value as namedTypes.FunctionExpression);
+                    node.comments = item.comments;
+                    methodNodes.push(node);
+                }
+            }
+        }
+    }
+
+    const vueNode = new VueNode(className!);
+    vueNode.props = propNodes;
+    vueNode.data = dataNodes;
+    vueNode.computed = computedNodes;
+    vueNode.watch = watchNodes;
+    vueNode.methods = methodNodes;
 }
--- src/utils/vue.ts
+++ src/utils/vue.ts
@@ -1,16 +1,19 @@
 import fs from 'fs';
 
 export function getScriptContent(code: string) {
-    const startPosJs = code.indexOf('<script>');
-    const startPosTs = code.indexOf('<script lang="ts">');
-    if (startPosJs < 0 && startPosTs < 0) {
+    let tag = '<script>';
+    let startPos = code.indexOf(tag);
+    if (startPos < 0) {
+        tag = '<script lang="ts">';
+        startPos = code.indexOf(tag);
+    }
+    if (startPos < 0) {
         return null;
     }
-    const startPos = startPosJs < 0 ? startPosTs : startPosJs;
     const endPos = code.indexOf('</script>');
     const header = code.substr(0, startPos) + '<script lang="ts">';
     const footer = code.substr(endPos);
-    const jsScript = code.substr(startPos + 8, endPos - startPos - 8);
+    const jsScript = code.substr(startPos + tag.length, endPos - startPos - tag.length);
 
     return { header, footer, jsScript };
 }
