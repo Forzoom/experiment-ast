cf42471e62c247578f27724a76f6b4aa196d2bcb
Forzoom <forzoom@163.com>
2020-02-21 09:59:39 +0800

    feat: 完善store中的逻辑

--- src/js/store.ts
+++ src/js/store.ts
@@ -12,6 +12,45 @@ import {
 // 尝试自定义扩展ast-types的定义
 import { builders as b, namedTypes } from 'ast-types';
 
+function handleModule(interfaceName: string, moduleObject: namedTypes.ObjectExpression) {
+    // 使用state生成interface
+    let interfacePropertyList: namedTypes.TSPropertySignature[] = [];
+    const stateList = moduleObject!.properties.filter(property => property.type === 'Property' && property.value.type === 'ObjectExpression' && property.key.type === 'Identifier' && property.key.name === 'state')
+    if (stateList.length == 1) {
+        const state = stateList[0] as namedTypes.Property;
+        const properties = (state.value as namedTypes.ObjectExpression).properties as namedTypes.Property[];
+        interfacePropertyList = properties.map(item => {
+            const signature = tsPropertySignature((item.key as namedTypes.Identifier).name, 'any');
+            signature.comments = item.comments;
+            return signature;
+        });
+    }
+
+    const identifierM = b.identifier('m');
+    identifierM.typeAnnotation = b.tsTypeAnnotation(b.tsTypeReference(b.identifier(`Module<${interfaceName}, RootState>`)));
+    const moduleDeclarationNode = b.variableDeclaration(
+        'const',
+        [
+            b.variableDeclarator(identifierM, moduleObject)
+        ]
+    );
+    
+    // ModuleState
+    const exportStateNode = b.exportNamedDeclaration(
+        b.tsInterfaceDeclaration(
+            b.identifier(interfaceName),
+            b.tsInterfaceBody(interfacePropertyList)
+        ),
+        [],
+        null
+    );
+
+    return [
+        exportStateNode,
+        moduleDeclarationNode,
+    ];
+}
+
 /**
  * 对于store文件进行处理
  */
@@ -37,67 +76,43 @@ export default function(input: string, output: string) {
             },
         },
     });
-    const generatedAst = recast.parse(''); // program这个内容，但是没有body，在body中会有ImportDeclaration和ExportDefaultDeclaration内容
     const interfaceName = camelCaseWithFirstLetter(fileName) + 'State';
     const importDeclarations: namedTypes.ImportDeclaration[] = [];
+    const body = originalAst.program.body;
+    let exportDefaultDeclaration: namedTypes.ExportDefaultDeclaration | null = null;
     let moduleObject: namedTypes.ObjectExpression | null = null;
+    const other: namedTypes.Node[] = [];
     
-    // 对于store进行处理
-    recast.visit(originalAst, {
-        // 处理import
-        visitImportDeclaration(d) {
-            importDeclarations.push(d.value);
-            this.traverse(d);
-        },
-        // 处理object
-        visitExportDefaultDeclaration(d) {
-            moduleObject = d.value.declaration as namedTypes.ObjectExpression;
-            this.traverse(d);
-        },
-    });
-
-    if (!moduleObject || moduleObject!.type !== 'ObjectExpression') {
-        console.info('cannot find module object');
+    for (const node of body as namedTypes.Node[]) {
+        if (node.type === 'ImportDeclaration') {
+            importDeclarations.push(node as namedTypes.ImportDeclaration);
+        } else if (node.type === 'ExportDefaultDeclaration') {
+            exportDefaultDeclaration = node as namedTypes.ExportDefaultDeclaration;
+        } else {
+            other.push(node);
+        }
+    }
+    
+    if (!exportDefaultDeclaration) {
+        console.warn('cannot find export default declaration');
         return;
     }
 
-    // 使用state生成interface
-    let interfacePropertyList: namedTypes.TSPropertySignature[] = [];
-    const stateList = moduleObject!.properties.filter(property => property.type === 'Property' && property.value.type === 'ObjectExpression' && property.key.type === 'Identifier' && property.key.name === 'state')
-    if (stateList.length == 1) {
-        const state = stateList[0] as namedTypes.Property;
-        const properties = (state.value as namedTypes.ObjectExpression).properties as namedTypes.Property[];
-        interfacePropertyList = properties.map(item => {
-            const signature = tsPropertySignature((item.key as namedTypes.Identifier).name, 'any');
-            signature.comments = item.comments;
-            return signature;
-        });
-    }
+    const declaration = exportDefaultDeclaration.declaration;
+    if (declaration.type === 'ObjectExpression') {
+        moduleObject = declaration as namedTypes.ObjectExpression;
 
-    const identifierM = b.identifier('m');
-    identifierM.typeAnnotation = b.tsTypeAnnotation(b.tsTypeReference(b.identifier(`Module<${interfaceName}, RootState>`)));
-    const moduleDeclarationNode = b.variableDeclaration(
-        'const',
-        [
-            b.variableDeclarator(identifierM, moduleObject)
-        ]
-    );
-    
-    const exportStateNode = b.exportNamedDeclaration(
-        b.tsInterfaceDeclaration(
-            b.identifier(interfaceName),
-            b.tsInterfaceBody(interfacePropertyList)
-        ),
-        [],
-        null
-    );
-    
-    generatedAst.program.body.push(...importDeclarations, importModuleFromVuex, importRootStateFromStoreDTS);
-    generatedAst.program.body.push(exportStateNode);
-    generatedAst.program.body.push(moduleDeclarationNode);
-    generatedAst.program.body.push(exportDefaultM);
-    const code = recast.print(generatedAst, {
-        tabWidth: 4,
-    }).code;
-    fs.writeFileSync(output, code);
+        const nodes = handleModule(interfaceName, moduleObject);
+        const generatedAst = recast.parse(''); // program这个内容，但是没有body，在body中会有ImportDeclaration和ExportDefaultDeclaration内容
+        generatedAst.program.body.push(...importDeclarations, importModuleFromVuex, importRootStateFromStoreDTS);
+        generatedAst.program.body.push(...other);
+        generatedAst.program.body.push(...nodes);
+        generatedAst.program.body.push(exportDefaultM);
+        const code = recast.print(generatedAst, {
+            tabWidth: 4,
+        }).code;
+        fs.writeFileSync(output, code);
+    } else {
+        fs.writeFileSync(output, originalCode);
+    }
 }
